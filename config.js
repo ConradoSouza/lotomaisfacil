35cb47cf162fbc803080a6290079bc0bdace1e46
Conrado Souza <287737317+ConradoSouza@users.noreply.github.com>
2026-08-27 11:02:25 -0300

    Push: adiciona a chave publica VAPID (habilita o toggle de avisos no app)

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ window.LOTO_CFG = {
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljbW5wYm5qY25raWRheXhiYXFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4NDYzMTMsImV4cCI6MjEwMTQyMjMxM30.6GzkdoesdrpiV3Dj5e9XJv_0jfZNrVRk3H2aGJMoEb4',
   // Chave PÚBLICA VAPID do Web Push (pode ficar aqui, é pública). Gere com: npx web-push generate-vapid-keys
   // Cole a "Public Key" abaixo. Enquanto estiver vazia, o botão de notificações fica oculto.
-  VAPID_PUBLIC: ''
+  VAPID_PUBLIC: 'BJLAG4t5oDesGdQLjjDLPaMrpWNHCcQcrQ1ZZFBtoZcWOPyrceL5eXsUNG4k0mV51UAveNPt-AHebZosnB9bu3A'
 };
